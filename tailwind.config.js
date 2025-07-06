bb0b780d1a6011732c0642f754b0363770d90b26
SamJam008 <soxxxji008@gmail.com>
2025-07-06 12:17:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+const {heroui} = require("@heroui/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/(alert|avatar|button|card|divider|dropdown|input|modal|progress|table|tabs|toast|popover|ripple|spinner|menu|form|checkbox|spacer).js"
+],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
