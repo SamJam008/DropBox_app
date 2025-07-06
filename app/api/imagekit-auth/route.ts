ed6f6db1a641e4b2cbe975e3699983b6652d721c
Somya Gupta <soxxxji008@gmail.com>
2025-07-06 20:37:46 +0530

    fix: console.log

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -1,13 +1,28 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import ImageKit from "imagekit"
-import { env } from "process";
+import { env } from "process"
 
-const imagekit = new ImageKit({
-    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
-    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENPOINT || ""
+const publicKey = process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "";
+const privateKey = process.env.IMAGEKIT_PRIVATE_KEY || "";
+const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || ""; // ✅ fix typo here too!
+
+// Debug the env values
+console.log("ENV CHECK", {
+    publicKey,
+    privateKey,
+    urlEndpoint,
 });
+
+// Initialize ImageKit
+const imagekit = new ImageKit({
+    publicKey,
+    privateKey,
+    urlEndpoint,
+});
+
+
+
 export async function GET() {
     try {
         const { userId } = await auth()
