9fec903aacc6d2a1d91b7ba74c48e6b4fafaad45
Somya Gupta <soxxxji008@gmail.com>
2025-07-24 19:35:31 +0530

    Added dotenv to git

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Providers } from "./provider";
+import { Providers } from "./providr";
 import "./globals.css";
 
 const inter = Inter({
